c1b37a286428354d142420baf61961d70bd2b5aa
David <dav.gevorgyann@gmail.com>
2022-06-30 22:20:28 +0400

    Добавьте файлы проекта.

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("David");
+            //Console.WriteLine("Gevorgyan");
+            //Console.Write("16");
+
+
+
+            //Console.WriteLine("Pizza Types");
+            //Console.WriteLine("pepperoni");
+            //Console.WriteLine("margherita");
+            //Console.WriteLine("veggie");
+
+
+
+            //int a = 2;
+            //int b = 6;
+            //int c = b / a;
+
+            //Console.WriteLine(c);
+
+
+
+            //int a = 2;
+            //int b = 10;
+            //int c = 16;
+            //int d = (b / a) + c;
+
+            //Console.WriteLine(d);
+
+
+
+            //int a = 5;
+            //int b = 19;
+            //int c = 48;
+            //int d = 21;
+            //int e = (a + b) - (c - d);
+
+            //Console.WriteLine(e);
+
+
+
+
+            //string Firstname = "David ";
+            //string Lastname = "Gevorgyan";
+            //int age = 16;
+
+            //Console.Write(Firstname);
+            //Console.WriteLine(Lastname);
+            //Console.Write(age);
+
+
+
+
+            //string name = "Nike Air Force";
+            //string s = "Size-";
+            //byte size = 42;
+
+            //Console.WriteLine(name);
+            //Console.Write(s);
+            //Console.Write(size);
+
+
+            //string a = "Ashot-";
+            //string k = "Karen-";
+            //string n = "Narek-";
+            //byte aa = 19;
+            //byte kk = 23;
+            //byte nn = 17;
+
+            //Console.Write(a);
+            //Console.WriteLine(aa);
+            //Console.Write(k);
+            //Console.WriteLine(kk);
+            //Console.Write(n);
+            //Console.Write(nn);
+
+
+
+
+
+
+            //string g = "Got Means +";
+            //string s = "Sold Means -";
+            //string a = "meat+";
+            //string b = "milk";
+            //string c = "bread";
+            //string d = "egg+";
+            //sbyte q = +20;
+            //sbyte w = +50;
+            //sbyte r = -15;
+            //sbyte t = -4;
+
+
+            //Console.WriteLine(g);
+            //Console.WriteLine(a + q);
+            //Console.WriteLine(d + w);
+            //Console.WriteLine(s);
+            //Console.WriteLine(b+t);
+            //Console.WriteLine(c+r);
+
+
+
+
+
+
+            //string a = "David's salary is $";
+            //ushort aa = 500;
+            //string b = "Emily's salary is $";
+            //ushort bb = 250;
+
+            //Console.WriteLine(a + aa);
+            //Console.WriteLine(b + bb);
+
+
+
+
+            //string name = "David";
+            //string lastname = "Gevorgyan";
+            //byte age = 16;
+            //byte weight = 55;
+            //bool isemployed = false;
+
+
+            //Console.WriteLine($"Name:{name}");
+            //Console.WriteLine($"Lastname:{lastname}");
+            //Console.WriteLine($"Age:{age}");
+            //Console.WriteLine($"Weight:{weight}");
+            //Console.WriteLine($"Works:{isemployed}");
+
+
+
+            //DateTime date1 = DateTime.Now;
+            //Console.WriteLine(date1);
+            
+        }
+    }
+}
