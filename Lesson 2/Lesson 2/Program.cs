50dedad8e3cecd8a9f82156aef35fb5149a2db0c
Dav Gevorgyan <108405180+davidgevorgyann@users.noreply.github.com>
2022-07-05 22:25:52 +0400

    Add files via upload

---
+++ Lesson 2/Lesson 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+            //double num2 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(num1 + num2);
+
+
+
+            //int a = 25;
+            //int b = 10;
+
+            //if (a>b)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+
+
+            //for(int a= 1; a< 5;a++)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+
+            //for (int a = 1,b= 1; a < 10; a++, b++)
+            //{
+            //    Console.WriteLine($"{a + b}");
+            //}
+
+
+            //for(int a = 1, b = 1; a < 10; a++, b++)
+            //{
+            //    Console.WriteLine($"{a * b}");
+            //}
+
+
+
+            //for (int i = 1;i<20;i*=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+        }
+    }
+}
