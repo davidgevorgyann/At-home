94c52066721e00006aa09cdc2c97d3e9a9331b3f
Dav Gevorgyan <108405180+davidgevorgyann@users.noreply.github.com>
2022-07-06 15:55:24 +0400

    Add files via upload

---
+++ Xndirner/Xndirner/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Xndirner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a =");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input b =");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input c =");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+
+            //xndir 21
+
+            //if(a>b  && c < a)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            //else if(b>a && c < b)
+            //{
+            //    Console.WriteLine(b);
+            //}
+
+            //else if(c>a  && b < c)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+
+            //xndir 22
+
+            //if(a<b && c > a)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            //else if(b<a && c > b)
+            //{
+            //    Console.WriteLine(b);
+            //}
+
+            //else if(c<b && a > c)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+
+            //xndir 23
+
+            //if (a == 1)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else if(b == 1)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+
+            //else if (c == 1)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+
+            //xndir 24
+
+            //if(a==2 && b == 2)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else if(b==2 && c == 2)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else if(c==2 && a == 2)
+            //{
+            //    Console.WriteLine(true);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine(false);
+            //}
+
+
+        }
+    }
+}
