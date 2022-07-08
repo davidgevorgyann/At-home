026e236a290afc135764758da339dc8c34db50bb
Dav Gevorgyan <108405180+davidgevorgyann@users.noreply.github.com>
2022-07-08 16:53:49 +0400

    Add files via upload

---
+++ Lesson 3/Lesson 3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ete naxapes arden gitenq obyekti chapy , uremn karanq grenq
+            //StringBuilder sb = new StringBuilder(32);
+
+            //u karanq miangamic grenq ham chapy ham toxy
+            //StringBuilder sb = new StringBuilder("David", 32);
+
+
+
+
+            //StringBuilder sb = new StringBuilder("David");
+            //var length = sb.Length;
+            //Console.WriteLine(length);
+            //var capa = sb.Capacity;
+            //Console.WriteLine(capa);
+
+
+
+
+            //StringBuilder-i hamar kan naev metodner
+            // .Append , .Insert , .Remove , .Replace , .AppendFormat 
+
+
+            //StringBuilder sb = new StringBuilder("Name:");
+            //Console.WriteLine(sb);     //Name
+            //Console.WriteLine($"Lenght: {sb.Length}");    //5
+            //Console.WriteLine($"Capacity: {sb.Capacity}");   //16
+
+
+            //sb.Append("David");
+            //Console.WriteLine(sb);    //Name:David
+            //Console.WriteLine($"Length: {sb.Length}");   //10
+            //Console.WriteLine($"Capacity: {sb.Capacity}");   //16
+
+
+            //sb.Append("Gevorgyan");
+            //Console.WriteLine(sb);   //Name:DavidGevorgyan
+            //Console.WriteLine($"Length: {sb.Length}");   //19
+            //Console.WriteLine($"Capacity: {sb.Capacity}");   //32
+
+
+
+
+
+
+
+            //StringBuilder sb = new StringBuilder("Hello World");
+            //sb.Append("!");
+            //sb.Insert(6, "My ");
+            //Console.WriteLine(sb); // Hello My World!
+
+            //sb.Replace("My", "Beautiful");
+            //Console.WriteLine(sb); // Hello Beautiful World!
+
+            //sb.Remove(15, 6);
+            //Console.WriteLine(sb); // Hello Beautiful!
+
+            //string text = sb.ToString();
+            //Console.WriteLine(text); // Hello Beautiful!
+
+
+
+
+
+
+            //string text = "ABCDEFGHIJKLMNOP";
+            //Console.WriteLine("the length of the text is:" + text.Contains('C'));
+            //// ete string-i mej text.Contains('?') drvac tary ka uremn true , urish false
+            //string u = text.Replace('A', 'M');
+            //Console.WriteLine(text);
+            //Console.WriteLine(u);
+
+      
+
+        }
+    }
+}
