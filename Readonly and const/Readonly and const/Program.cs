f5e05022f31d64348329b0e99c10c6667a23752f
Dav Gevorgyan <108405180+davidgevorgyann@users.noreply.github.com>
2022-07-13 11:52:26 +0400

    Add files via upload

---
+++ Readonly and const/Readonly and const/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Readonly_and_const
+{
+    class Myclass
+    {
+        public const double PI = 3.14;
+        public const string ERROR = "some error";
+
+        public readonly int _a;
+
+        public static readonly int _b;
+
+
+        static Myclass()
+        {
+            _b= 3;
+        }
+
+
+    public Myclass(int a)
+    {
+            _a = a;
+    }
+     public void Abc()
+        {
+            Console.WriteLine(PI);
+            Console.WriteLine(_a);
+            Console.WriteLine(_b);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass myclass = new Myclass(4);
+            myclass.Abc();
+
+        }
+    }
+}
